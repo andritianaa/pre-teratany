14c18ea5ae847ec4d4da1f6ce59a7ff6f1b48bec
NyHasina <nyhasina.finaritra@gmail.com>
2024-01-29 19:37:09 +0300

    amelioration code following profiles

---
+++ src/store/reducer/profile.reducer.ts
@@ -0,0 +1,62 @@
+import { withAsync } from "../../helpers/withAsync";
+import { ErrorData, ThrowErrorHandler } from "../../helpers/HandleError";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { IProfile } from "../../types/profile.type";
+import { getFollowedProfile } from "../../api/ProfileApi";
+
+type ProfileInitialState = {
+    loading: boolean,
+    error: string,
+    followed_profiles: IProfile[]
+}
+
+const initialState: ProfileInitialState = {
+    loading: false,
+    error: "",
+    followed_profiles: []
+}
+
+export interface ProfileProperty {
+    token: string,
+    profileId: string
+}
+
+export const fetchFollowedProfiles = createAsyncThunk('followed_profile/fetch', async (profile_property: ProfileProperty, { rejectWithValue }) => {
+
+    const { error, response } = await withAsync(() =>
+        getFollowedProfile(profile_property.token, profile_property.profileId!)
+    );
+    if (error) {
+        ThrowErrorHandler(error as ErrorData);
+        return rejectWithValue(error)
+    } else {
+        return response?.data as IProfile[]
+    }
+})
+
+export const profileSlice = createSlice({
+    name: "profiles",
+    initialState,
+    reducers: {
+
+    },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchFollowedProfiles.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchFollowedProfiles.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            state.followed_profiles = payload as IProfile[];
+        });
+        builder.addCase(fetchFollowedProfiles.rejected, (state, { payload }) => {
+            state.loading = false;
+            state.error = String(payload);
+        });
+
+    }
+
+})
+
+// export const {  } = profileSlice.actions;
+export default profileSlice.reducer;
--- src/views/Page/PageTopList.tsx
+++ src/views/Page/PageTopList.tsx
@@ -1,77 +1,44 @@
-import { AxiosError } from "axios";
-import { getFollowedProfile } from "../../api/ProfileApi";
-import { withAsync } from "../../helpers/withAsync";
-import useFetchProfile from "../../hooks/useFetchProfile";
-import useToken from "../../hooks/useToken";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import { IProfile } from "../../types/profile.type";
 import { FileServerURL } from "../../api/FileApi";
 import { Link } from "react-router-dom";
 import FeedNoPage from "../NoPage/FeedNoPage";
 import profileDefault from "../../assets/userPics.jpg";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { IProfile } from "../../types/profile.type";
 
 const PageTopList = () => {
-  const profileConnectedUser = useFetchProfile();
-  const token = useToken();
-  const [profileFollowed, setProfileFollowed] = useState<IProfile[]>();
-  const [isProfileFetched, setIsProfileFetched] = useState<Boolean>(false);
-
-  const fetchFollowedProfile = async () => {
-    if (profileConnectedUser) {
-      const { error, response } = await withAsync(() =>
-        getFollowedProfile(token, profileConnectedUser?._id! ?? "")
-      );
-      if (error instanceof AxiosError) {
-        const error_message: string =
-          error?.response?.data.description ||
-          error?.response?.data ||
-          error.message;
-        toast.error(error_message);
-      } else {
-        setProfileFollowed(response?.data as IProfile[]);
-        setIsProfileFetched(true);
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchFollowedProfile();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [profileConnectedUser?._id]);
-
-  console.log(profileFollowed);
+  const profileFollowed = useSelector<RootState>(
+    (state) => state.teratany_profiles.followed_profiles
+  ) as IProfile[];
 
   return (
     <>
-      {isProfileFetched && (
-        <>
-          {profileFollowed?.length! > 0 ? (
-            <div className="flex my-2 overflow-x-scroll no-scrollbar  w-full sm:w-[30%] pt-2 pb-0 pl-4 ">
-              {profileFollowed?.map((profile) => (
-                <div className="flex flex-col items-center mr-4">
-                  <Link to={`/profile/${profile?._id}`}>
-                    <img
-                      className="w-16 h-16 max-w-16 max-h-16 min-w-[3rem] min-h-[3rem] p-0.5 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 object-cover"
-                      src={
-                        profile?.image
-                          ? FileServerURL + profile?.image
-                          : profileDefault
-                      }
-                      alt="profile"
-                    />
-                  </Link>
-                  <p className="truncated-name text-center">{profile?.name}</p>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="flex justify-center items-center h-[calc(100vh-8rem)]">
-              <FeedNoPage />
-            </div>
-          )}
-        </>
-      )}
+      <>
+        {profileFollowed?.length! > 0 ? (
+          <div className="flex my-2 overflow-x-scroll no-scrollbar  w-full sm:w-[30%] pt-2 pb-0 pl-4 ">
+            {profileFollowed?.map((profile) => (
+              <div className="flex flex-col items-center mr-4">
+                <Link to={`/profile/${profile?._id}`}>
+                  <img
+                    className="w-16 h-16 max-w-16 max-h-16 min-w-[3rem] min-h-[3rem] p-0.5 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 object-cover"
+                    src={
+                      profile?.image
+                        ? FileServerURL + profile?.image
+                        : profileDefault
+                    }
+                    alt="profile"
+                  />
+                </Link>
+                <p className="truncated-name text-center">{profile?.name}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-[calc(100vh-8rem)]">
+            <FeedNoPage />
+          </div>
+        )}
+      </>
     </>
   );
 };
